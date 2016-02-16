c4940d7e7fe3f06cd97db90e14dd394b422ee576
Dale Bustad <dale@divmain.com>
2016-02-16 14:45:17 -0800

    Prepare victory-chart for monolith merge.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  VictoryChart: require("./components/victory-chart/victory-chart"),
-  VictoryLine: require("./components/victory-line/victory-line"),
-  VictoryAxis: require("./components/victory-axis/victory-axis"),
-  VictoryBar: require("./components/victory-bar/victory-bar"),
-  VictoryScatter: require("./components/victory-scatter/victory-scatter")
-};
